9126f19eb1adf05e4bc263f396cb982288c540ce
nya0 <nya0@duck.com>
2025-09-28 13:18:44 +0300

    add other funcs to header

--- include/parse.h
+++ include/parse.h
@@ -17,8 +17,10 @@ typedef struct {
     unsigned short hours;
 } employee_t;
 
-int create_db_header(int fd, dbheader_t **headerOut);
-int validate_db_header(int fd, dbheader_t **headerOut);
+int create_db_header(dbheader_t **headerOut);
+int validate_db_header(int fd,dbheader_t **headerOut);
+int read_employees(int fd, dbheader_t *, employee_t **employeesOut);
+int output_file(int fd, dbheader_t *, employee_t *employees);
 
 
 # endif
